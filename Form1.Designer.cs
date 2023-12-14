7706856209bd1b84df1399c05c50623bffd89f1a
christineR745 <136216444+christineR745@users.noreply.github.com>
2023-12-14 15:54:43 -0700

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_combatArea4 = new System.Windows.Forms.Button();
+            this.btn_combatArea3 = new System.Windows.Forms.Button();
+            this.btn_combatArea2 = new System.Windows.Forms.Button();
+            this.btn_combatArea1 = new System.Windows.Forms.Button();
+            this.btn_shop = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_combatArea4
+            // 
+            this.btn_combatArea4.BackColor = System.Drawing.Color.Transparent;
+            this.btn_combatArea4.Enabled = false;
+            this.btn_combatArea4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_combatArea4.Location = new System.Drawing.Point(217, 313);
+            this.btn_combatArea4.Name = "btn_combatArea4";
+            this.btn_combatArea4.Size = new System.Drawing.Size(73, 66);
+            this.btn_combatArea4.TabIndex = 0;
+            this.btn_combatArea4.UseVisualStyleBackColor = false;
+            this.btn_combatArea4.Visible = false;
+            // 
+            // btn_combatArea3
+            // 
+            this.btn_combatArea3.BackColor = System.Drawing.Color.Transparent;
+            this.btn_combatArea3.Enabled = false;
+            this.btn_combatArea3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_combatArea3.Location = new System.Drawing.Point(380, 409);
+            this.btn_combatArea3.Name = "btn_combatArea3";
+            this.btn_combatArea3.Size = new System.Drawing.Size(73, 66);
+            this.btn_combatArea3.TabIndex = 1;
+            this.btn_combatArea3.UseVisualStyleBackColor = false;
+            this.btn_combatArea3.Visible = false;
+            // 
+            // btn_combatArea2
+            // 
+            this.btn_combatArea2.BackColor = System.Drawing.Color.Transparent;
+            this.btn_combatArea2.Enabled = false;
+            this.btn_combatArea2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_combatArea2.Location = new System.Drawing.Point(192, 474);
+            this.btn_combatArea2.Name = "btn_combatArea2";
+            this.btn_combatArea2.Size = new System.Drawing.Size(73, 66);
+            this.btn_combatArea2.TabIndex = 2;
+            this.btn_combatArea2.UseVisualStyleBackColor = false;
+            this.btn_combatArea2.Visible = false;
+            // 
+            // btn_combatArea1
+            // 
+            this.btn_combatArea1.BackColor = System.Drawing.Color.Transparent;
+            this.btn_combatArea1.Enabled = false;
+            this.btn_combatArea1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_combatArea1.Location = new System.Drawing.Point(498, 525);
+            this.btn_combatArea1.Name = "btn_combatArea1";
+            this.btn_combatArea1.Size = new System.Drawing.Size(73, 66);
+            this.btn_combatArea1.TabIndex = 3;
+            this.btn_combatArea1.UseVisualStyleBackColor = false;
+            this.btn_combatArea1.Visible = false;
+            // 
+            // btn_shop
+            // 
+            this.btn_shop.BackColor = System.Drawing.Color.Transparent;
+            this.btn_shop.Enabled = false;
+            this.btn_shop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_shop.Location = new System.Drawing.Point(180, 708);
+            this.btn_shop.Name = "btn_shop";
+            this.btn_shop.Size = new System.Drawing.Size(73, 83);
+            this.btn_shop.TabIndex = 4;
+            this.btn_shop.UseVisualStyleBackColor = false;
+            this.btn_shop.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::OOP_FinalProject.Properties.Resources.worldMap;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 949);
+            this.Controls.Add(this.btn_shop);
+            this.Controls.Add(this.btn_combatArea1);
+            this.Controls.Add(this.btn_combatArea2);
+            this.Controls.Add(this.btn_combatArea3);
+            this.Controls.Add(this.btn_combatArea4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +115,12 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_combatArea4;
+        private System.Windows.Forms.Button btn_combatArea3;
+        private System.Windows.Forms.Button btn_combatArea2;
+        private System.Windows.Forms.Button btn_combatArea1;
+        private System.Windows.Forms.Button btn_shop;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,15 @@ namespace OOP_FinalProject
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // create map and location objects
+            WorldMap map = new WorldMap();
+            Location combatArea1 = new Location("combatArea1", btn_combatArea1, 25);
+            Location combatArea2 = new Location("combatArea2", btn_combatArea2, 50);
+            Location combatArea3 = new Location("combatArea3", btn_combatArea3, 50);
+            Location combatArea4 = new Location("combatArea4", btn_combatArea4, 100);
+            Location shop = new Location("shop", btn_shop);
 
+            map.Locations = new List<Location>{ combatArea1, combatArea2, combatArea3, combatArea4, shop };
         }
     }
 }
