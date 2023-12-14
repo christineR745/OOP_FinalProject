351a729afad969d67b1f6bbbd7cd33f990bcaa20
christineR745 <136216444+christineR745@users.noreply.github.com>
2023-12-13 21:11:28 -0700

    Create Location.cs

---
+++ Location.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_FinalProject
+{
+    public class Location
+    {
+        public string Name;
+        public Button Button;
+        public List<Enemy> Enemies;
+        public List<Item> ItemDrops;
+        public int maxCoinDrop;
+
+        public void LoadLocation()
+        {
+            // randomly choose enemies from list
+            var random = new Random();
+            int randInt = random.Next(Enemies.Count);
+            Enemy randEnemy = Enemies[randInt];
+
+            // combat method that automatically runs, using randEnemy
+
+            // roll for random item drop, just made it 60% chance
+            randInt = random.Next(10);
+            if(randInt < 7)
+            {
+                randInt = random.Next(ItemDrops.Count);
+                // put the item in player's item bag
+            }
+
+            // give player coins
+            randInt = random.Next(maxCoinDrop);
+            // add the result to player's coins
+        }
+    }
+}
