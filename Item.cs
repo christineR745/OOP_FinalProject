50be4048a514b3772b0adb8c5eec277e469c0ab0
christineR745 <136216444+christineR745@users.noreply.github.com>
2023-12-14 22:37:19 -0700

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace OOP_FinalProject
 {
-    internal class Item
+    public class Item
     {
+        public string Name;
+        public string Description;
+    }
+
+    public class Equipable : Item 
+    {
+        public int Armor;
+        public int Damage;
+    
+        public Equipable(string name, int armor = 0, int damage = 0)
+        {
+            Name = name;
+            Armor = armor;
+            Damage = damage;
+        }
+    }
+
+    public class Consumable : Item 
+    {
+        public int Uses;
+        public int Charge;
+    
+        public Consumable(string name, int uses = 0, int charge = 0)
+        {
+            Name = name;
+            Uses = uses;
+            Charge = charge;
+        }
+    
+        public virtual void Effect() 
+        {
+            
+        }
     }
 }
